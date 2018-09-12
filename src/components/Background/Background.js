d03cd8194a211c01818a37a1bcb79ec7a68146cc
oahmaro <osamaahmaro@gmail.com>
2018-09-12 10:15:41 +0300

    feat: Create Background Component

---
+++ src/components/Background/Background.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css, cx } from 'react-emotion'
+
+const dynamicStyles = ({ theme }) => css`
+  background: ${theme.styles.ui.fill3};
+`
+
+const StyledBackground = styled('div')`
+  display: flex;
+  box-sizing: border-box;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+  position: absolute;
+  ${dynamicStyles}
+`
+
+const Background = (props) => {
+  return (
+    <StyledBackground {...props} style={props.style} className={cx(props.className)} />
+  )
+}
+
+Background.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object
+}
+
+export default Background
