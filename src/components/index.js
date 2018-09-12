e76493241b54a39fb8a848b76cb2d2c9d0e3405c
oahmaro <osamaahmaro@gmail.com>
2018-09-12 12:47:46 +0300

    feat: Create Text Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Rhino } from './Rhino'
 export { default as Button } from './Button'
 export { default as Background } from './Background'
+export { default as Text } from './Text'
