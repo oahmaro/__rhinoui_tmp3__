8d58db9fafc0d33f83dcca28a3bdd7e3cdf1a2c0
oahmaro <osamaahmaro@gmail.com>
2018-09-13 12:51:35 +0300

    feat: Create Box Component

---
+++ src/components/Box/Box.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'react-emotion'
+// import { withTheme } from 'emotion-theming'
+
+const StyledBox = styled('div')`
+  display: flex;
+  background: black;
+  z-index: 1;
+  box-sizing: border-box;
+`
+
+const Box = (props) => {
+  return (
+    <StyledBox {...props}>
+      { props.children }
+    </StyledBox>
+  )
+}
+
+Box.propTypes = {
+  children: PropTypes.node
+}
+
+export default Box
